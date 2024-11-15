88b48ec323e776e757349cb0d7c7d90be178ca1f
Bozhidar1804 <bobo1804@abv.bg>
2024-11-15 16:50:26 +0200

    Test commit to see if .gitignore finally works correctly

--- ProjectHub/ProjectHub.Data/obj/Debug/net8.0/ProjectHub.Data.AssemblyInfo.cs
+++ ProjectHub/ProjectHub.Data/obj/Debug/net8.0/ProjectHub.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectHub.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d7e0422ccea95b338bb29db6525587890ca909")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad4bdc854850f1477471d6885fc07809a42188c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectHub.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectHub.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
