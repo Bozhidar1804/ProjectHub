975705353d2b5c54614d4243583847a7fe0513e3
Bozhidar1804 <bobo1804@abv.bg>
2024-11-18 23:25:48 +0200

    Implemented ClaimsPrincipalExtensions for future use

---
+++ ProjectHub/ProjectHub.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+using static ProjectHub.Common.GeneralApplicationConstants;
+
+namespace ProjectHub.Web.Infrastructure.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRoleName);
+        }
+    }
+}
